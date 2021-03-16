1d069a6af229d31da0a47fe7daaa05ba8bb417ed
Prateek Varma <prtk.varma@gmail.com>
2021-03-16 17:12:43 -0600

    Added second argument to useEffect function to clear memory clutter

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ function App() {
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     }
-  });
+  }, [count]);
+
+  // Above, you can pass an empty array at the end of the return, this will ensure the function (useEffect) is run only when component is mounted, and then unmounted
+  // But the empty array will not display click count on the browser tab title. To fix this, pass 'count' into the array - to run function every time count is changed.
 
   const handleMouseMove = event => {
     setMousePosition({
