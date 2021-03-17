b194a4eeebbf51054f010e738a66218418b47a0c
Prateek Varma <prtk.varma@gmail.com>
2021-03-16 18:13:27 -0600

    check internet online, using the navigator.onLine browser API

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ function App() {
   const [count, setCount] = useState(0)
   const [isOn, setIsOn] = useState(false)
   const [mousePosition, setMousePosition] = useState({x: null, y: null})
+  const [status, setStatus] = useState(navigator.onLine)
 
   useEffect(() => {
     document.title = `Clicked ${count} times`;
     window.addEventListener('mousemove', handleMouseMove);
-    // the following will remove the memory clutter
-    // it won't just clean after running the effect, but also before running an effect
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+    // the following will remove the memory clutter, it won't just clean after running the effect, but also before running an effect
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline); 
     }
   }, [count]);
-
   // Above, you can pass an empty array at the end of the return, this will ensure the function (useEffect) is run only when component is mounted, and then unmounted
   // But the empty array will not display click count on the browser tab title. To fix this, pass 'count' into the array - to run function every time count is changed.
+
+  const handleOnline = () => {
+    setStatus(true)
+  }
+
+  const handleOffline = () => {
+    setStatus(false)
+  }
 
   const handleMouseMove = event => {
     setMousePosition({
@@ -40,6 +51,7 @@ function App() {
     <div onClick={changeColor} style={{ height:"50px", width:"50px", background: isOn ? "yellow" : "gray" }}></div>
     <h4>Mouse Coordinates:</h4>
     {JSON.stringify(mousePosition)}
+    <h4>Internet Status:</h4><span>{status ? "online" : "offline"}</span>
     </>
   );
 }
